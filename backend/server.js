5cfd4da388526ed114eab0e5e7ae60d02db83eea
Digvijay <shahidigvijay144@gmail.com>
2026-09-21 12:36:27 +0530

    configure Vercel services

--- backend/server.js
+++ backend/server.js
@@ -25,15 +25,19 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, error: 'Server Error' });
 });
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  // Handle unhandled promise rejections
+  process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+  });
+}
 
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  // Close server & exit process
-  server.close(() => process.exit(1));
-});
+module.exports = app;
+
